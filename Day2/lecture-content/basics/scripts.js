9839aa144ba509e98fa6b32f646aeb71a7581deb
yuvraj3905 <74104618+Yuvraj3905@users.noreply.github.com>
2023-06-19 23:45:27 +0530

    Updated with Basics

---
+++ Day2/lecture-content/basics/scripts.js
@@ -0,0 +1,148 @@
+//! ===================================  INTRO  ====================================
+
+// document.write('hello');
+// console.log('BASIC JS');
+
+// ?variables
+var x = 45;
+console.log(x);
+let y = 34;
+y = 100;
+console.log(y);
+
+// ?constant
+// const z = 56;
+// z = 666;
+// console.log(z);
+
+//! ==================================== DATA TYPES  ======================================
+
+// let x = true
+// console.log(typeof y);
+
+// ! ================================== TYPES OF OPERATORS ==============================
+
+// ? A. ARITHMETIC OPERATORS
+// * Addition ->  (+)
+// * Subtraction -> (-)
+// * Multiplication ->  (*)
+//  * Division -> (/)
+//  * Modulus(%) ->
+// * Exponention -> (**)
+
+// let x = 12;
+// let y = 8;
+// document.write( 2 ** 3 );
+
+// ? RELATIONAL OPERATORS  => return true or false
+// * Equal to (==) -> check only value
+// * Not Equal to (!=)  -> check only value
+// * Greater than (>)
+// * Greater than or equal to (>=)
+// * Less than (<)
+// * Less than or equal to (<=)
+// * Strict equal ( === ) -> check data type + value
+// * Strict Not equal ( !== )  -> check data type + value
+
+// let a = 10;
+// console.log(a > 56);
+
+//  ? ASSIGNMENT OPERATORS
+//  * Assignment -> =
+//  * Addition Assignment -> +=
+// * Subtraction Assignment -> -=
+// * Multiplication Assignment -> *=
+// * Division Assignment -> /=
+// * Modulous Assignment -> %=
+// * Exponentiation Assignment -> **=
+
+// let a = 15;
+// a /= 3;  // a = a*3
+// document.write(a)
+
+// let r = 23;
+// r += 3; // r = r + 3
+// console.log(r);
+
+//  ? LOGICAL OPERATORS
+// * Logical AND -> &&
+// * Logical OR -> ||
+// * Logical NOT -> !
+
+// ! ================================ CONDITIONAL STATEMENTS =========================
+
+// * IF STATEMENT
+// ? SYNTAX ------------
+// if(condition){
+// statements
+// }
+
+// * IF-ELSE STATEMENT
+// ? SYNTAX ------------
+// if(condition){
+//     will execute when conditon is true
+// }else{
+//     will execute when condition is false
+// }
+
+// * IF-ELSEIF-ELSE STATEMENT
+// ? SYNTAX ------------
+// if(condition){
+//     statement1
+// }else if{
+//    statement2
+// }else{
+//     statement3
+// }
+
+// let w = -12;
+// if (w > 0) {
+//   console.log('positive');
+// } else if (w < 0) {
+//   console.log('negative');
+// } else {
+//   console.log('Zero');
+// }
+
+//! ====================================== LOOPS ===========================================
+
+// * for loop
+
+// ? SYNTAX ------------
+// for (initialization; condition; updation) {
+// statement
+// }
+
+// for(let i = 0;i<5;i++){
+//    console.log(i);
+// }
+
+// * While loop
+
+//  ? SYNTAX ------------
+// initialization
+// while(condition){
+//     code...
+//   updation
+//  }
+
+// let i = 0
+
+// while(i<5){
+//    console.log(i)
+//    i++
+// }
+
+// * DO-WHILE LOOP
+
+//  ? SYNATX -----------------
+
+// do {
+// Statement(s);
+//  } while (condition);
+
+// let i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 5);
